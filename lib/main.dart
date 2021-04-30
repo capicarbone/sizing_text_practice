e78a12d5b0c6a38d30f66434294193de2c0862db
Carlos Pinelly <cpinelly@gmail.com>
2021-04-29 19:34:04 -0500

    Basic logic for resize text until fit in box.

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Auto Text Test'),
     );
   }
+}
+
+class QuotePainter extends CustomPainter {
+  TextSpan text;
+  BoxConstraints constraints;
+
+  QuotePainter({this.text, this.constraints});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    var painter = TextPainter(
+        text: text ,
+        textDirection: TextDirection.ltr,
+        textAlign: TextAlign.center,
+        textScaleFactor: 1
+    );
+
+    painter.layout(minWidth: constraints.minWidth, maxWidth: constraints.maxWidth);
+
+    var adjusted = false;
+    do{
+      adjusted = painter.width < constraints.maxWidth && painter.height < constraints.maxHeight;
+
+      if (!adjusted) {
+        var currentFontSize = painter.text.style.fontSize;
+        var newText = TextSpan(text: text.text, style: text.style.copyWith(fontSize: (currentFontSize - 12)*0.80 + 12));
+        painter.text = newText;
+        painter.layout(minWidth: constraints.minWidth, maxWidth: constraints.maxWidth);
+      }
+
+    }while(!adjusted && painter.text.style.fontSize >= 12);
+
+    painter.paint(canvas, Offset.zero);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+
+    return true;
+  }
+
 }
 
 class MyHomePage extends StatelessWidget {
   String title;
   MyHomePage({Key key, this.title}) : super(key: key);
+
+  var testText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
 
   @override
   Widget build(BuildContext context) {
@@ -34,17 +77,18 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-
-          ],
+        child: Container(
+          width: double.infinity,
+          height: 200,
+          color: Colors.amber,
+          child: Padding(
+            padding: const EdgeInsets.all(22.0),
+            child: LayoutBuilder(builder: (context, constraints) {
+              return CustomPaint(painter: QuotePainter(text: TextSpan(text: testText, style: TextStyle(fontSize: 30)), constraints: constraints));
+            },) ,)
+            //Text(testText, style: TextStyle(fontSize: 50), textAlign: TextAlign.center,),
+          ),
         ),
-      )
     );
   }
 }
